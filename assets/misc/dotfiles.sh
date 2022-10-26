e6715b5477ff3f81120e915609fa2e08d82f66aa
Olivier Roques <oroques@fastmail.com>
2022-10-26 14:55:59 +0200

    refactor: dotfiles script

--- assets/misc/dotfiles.sh
+++ assets/misc/dotfiles.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 function install() {
   local user
   local home
@@ -12,15 +14,26 @@ function install() {
   apt-get update
   apt-get install -y git make
 
+  # Fetch dotfiles
   pushd "$home" > /dev/null
     sudo -u "$user" mkdir -p Documents Downloads .tmp
-    rm -rf .local/share/bash .bashrc .gitconfig .config/nvim .vimrc Documents/dotfiles
+    rm -rf Documents/dotfiles
     sudo -E -u "$user" git clone https://github.com/ojroques/dotfiles.git Documents/dotfiles
   popd > /dev/null
 
+  # Install packages
   pushd "$home"/Documents/dotfiles > /dev/null
     make install-cli
     make clean
+  popd > /dev/null
+
+  # Remove old dotfiles
+  pushd "$home" > /dev/null
+    rm -rf .local/share/bash .bashrc .gitconfig .config/nvim .vimrc
+  popd > /dev/null
+
+  # Install dotfiles
+  pushd "$home"/Documents/dotfiles > /dev/null
     sudo -u "$user" stow bash git nvim vim
   popd > /dev/null
 }
